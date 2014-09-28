9986a278b1b8f96b6db0d85f4be9f6c746dabf51
Rezoner <rezoner1337@gmail.com>
2014-09-28 19:42:12 +0200

    video recording

--- playground.js
+++ playground.js
@@ -63,6 +63,7 @@ function Playground(args) {
   if (!this.container) this.container = document.body;
   if (this.container !== document.body) this.customContainer = true;
   if (typeof this.container === "string") this.container = document.querySelector(this.container);
+
   /* state */
 
   this.state = {};
@@ -126,6 +127,10 @@ function Playground(args) {
 
   setTimeout(this.resizeHandler.bind(this), 1);
 
+  /* video recorder */
+
+  this.videoRecorder = new playground.VideoRecorder(this);
+
   /* game loop */
 
   var self = this;
@@ -141,16 +146,18 @@ function Playground(args) {
 
     if (delta > 1000) return;
 
+    var dt = delta / 1000;
+
     if (self.loader.count <= 0) {
 
-      if (self.step) self.step(delta / 1000);
-      if (self.render) self.render(delta / 1000);
+      if (self.step) self.step(dt);
+      if (self.render) self.render(dt);
 
-      if (self.state.step) self.state.step(delta / 1000);
-      if (self.state.render) self.state.render(delta / 1000);
+      if (self.state.step) self.state.step(dt);
+      if (self.state.render) self.state.render(dt);
 
     } else {
-      self.renderLoader(delta / 1000);
+      self.renderLoader(dt);
     }
 
     if (self.scaleToFit) {
@@ -162,7 +169,8 @@ function Playground(args) {
       self.screen.restore();
     }
 
-    self.gamepads.step();
+    self.gamepads.step(dt);
+    self.videoRecorder.step(dt);
 
   };
 
@@ -192,6 +200,7 @@ function Playground(args) {
 
     self.ready = function() {};
   });
+
 
 
 };
@@ -290,6 +299,9 @@ Playground.prototype = {
 
   },
 
+  record: function(args) {
+    this.videoRecorder.toggle(args);
+  },
 
   /* imaginary timeout to delay loading */
 
@@ -436,7 +448,7 @@ Playground.prototype = {
       var layer = cq(32, 32);
       layer.font("10px " + name).fillText(16, 16, 16);
       layer.trim();
-      
+
       if (layer.width !== width || layer.height !== height) {
         self.loader.ready("font " + name);
       } else {
@@ -976,7 +988,7 @@ playground.Gamepads.prototype = {
       /* hack for missing  dpads */
 
       for (var h = 12; h <= 15; h++) {
-        if(!buttons[h]) buttons[h] = {
+        if (!buttons[h]) buttons[h] = {
           pressed: false,
           value: 0
         };
@@ -1079,4 +1091,333 @@ playground.extend(playground.Loader.prototype, playground.Events.prototype);
 CanvasQuery.Layer.prototype.playground = function(args) {
   args.layer = this;
   return playground(args);
+};
+
+/* Video recorder */
+
+/* whammy - https://github.com/antimatter15/whammy */
+
+window.Whammy = function() {
+  function h(a, b) {
+    for (var c = r(a), c = [{
+      id: 440786851,
+      data: [{
+        data: 1,
+        id: 17030
+      }, {
+        data: 1,
+        id: 17143
+      }, {
+        data: 4,
+        id: 17138
+      }, {
+        data: 8,
+        id: 17139
+      }, {
+        data: "webm",
+        id: 17026
+      }, {
+        data: 2,
+        id: 17031
+      }, {
+        data: 2,
+        id: 17029
+      }]
+    }, {
+      id: 408125543,
+      data: [{
+        id: 357149030,
+        data: [{
+          data: 1E6,
+          id: 2807729
+        }, {
+          data: "whammy",
+          id: 19840
+        }, {
+          data: "whammy",
+          id: 22337
+        }, {
+          data: s(c.duration),
+          id: 17545
+        }]
+      }, {
+        id: 374648427,
+        data: [{
+          id: 174,
+          data: [{
+            data: 1,
+            id: 215
+          }, {
+            data: 1,
+            id: 25541
+          }, {
+            data: 0,
+            id: 156
+          }, {
+            data: "und",
+            id: 2274716
+          }, {
+            data: "V_VP8",
+            id: 134
+          }, {
+            data: "VP8",
+            id: 2459272
+          }, {
+            data: 1,
+            id: 131
+          }, {
+            id: 224,
+            data: [{
+              data: c.width,
+              id: 176
+            }, {
+              data: c.height,
+              id: 186
+            }]
+          }]
+        }]
+      }]
+    }], e = 0, d = 0; e < a.length;) {
+      var g = [],
+        f = 0;
+      do g.push(a[e]), f += a[e].duration, e++; while (e < a.length && 3E4 > f);
+      var h = 0,
+        g = {
+          id: 524531317,
+          data: [{
+            data: d,
+            id: 231
+          }].concat(g.map(function(a) {
+            var b = t({
+              discardable: 0,
+              frame: a.data.slice(4),
+              invisible: 0,
+              keyframe: 1,
+              lacing: 0,
+              trackNum: 1,
+              timecode: Math.round(h)
+            });
+            h += a.duration;
+            return {
+              data: b,
+              id: 163
+            }
+          }))
+        };
+      c[1].data.push(g);
+      d += f
+    }
+    return m(c, b)
+  }
+
+  function r(a) {
+    for (var b = a[0].width, c = a[0].height, e = a[0].duration,
+      d = 1; d < a.length; d++) {
+      if (a[d].width != b) throw "Frame " + (d + 1) + " has a different width";
+      if (a[d].height != c) throw "Frame " + (d + 1) + " has a different height";
+      if (0 > a[d].duration || 32767 < a[d].duration) throw "Frame " + (d + 1) + " has a weird duration (must be between 0 and 32767)";
+      e += a[d].duration
+    }
+    return {
+      duration: e,
+      width: b,
+      height: c
+    }
+  }
+
+  function u(a) {
+    for (var b = []; 0 < a;) b.push(a & 255), a >>= 8;
+    return new Uint8Array(b.reverse())
+  }
+
+  function n(a) {
+    var b = [];
+    a = (a.length % 8 ? Array(9 - a.length % 8).join("0") : "") + a;
+    for (var c = 0; c < a.length; c += 8) b.push(parseInt(a.substr(c,
+      8), 2));
+    return new Uint8Array(b)
+  }
+
+  function m(a, b) {
+    for (var c = [], e = 0; e < a.length; e++) {
+      var d = a[e].data;
+      "object" == typeof d && (d = m(d, b));
+      "number" == typeof d && (d = n(d.toString(2)));
+      if ("string" == typeof d) {
+        for (var g = new Uint8Array(d.length), f = 0; f < d.length; f++) g[f] = d.charCodeAt(f);
+        d = g
+      }
+      f = d.size || d.byteLength || d.length;
+      g = Math.ceil(Math.ceil(Math.log(f) / Math.log(2)) / 8);
+      f = f.toString(2);
+      f = Array(7 * g + 8 - f.length).join("0") + f;
+      g = Array(g).join("0") + "1" + f;
+      c.push(u(a[e].id));
+      c.push(n(g));
+      c.push(d)
+    }
+    return b ? (c = p(c), new Uint8Array(c)) :
+      new Blob(c, {
+        type: "video/webm"
+      })
+  }
+
+  function p(a, b) {
+    null == b && (b = []);
+    for (var c = 0; c < a.length; c++) "object" == typeof a[c] ? p(a[c], b) : b.push(a[c]);
+    return b
+  }
+
+  function t(a) {
+    var b = 0;
+    a.keyframe && (b |= 128);
+    a.invisible && (b |= 8);
+    a.lacing && (b |= a.lacing << 1);
+    a.discardable && (b |= 1);
+    if (127 < a.trackNum) throw "TrackNumber > 127 not supported";
+    return [a.trackNum | 128, a.timecode >> 8, a.timecode & 255, b].map(function(a) {
+      return String.fromCharCode(a)
+    }).join("") + a.frame
+  }
+
+  function q(a) {
+    for (var b = a.RIFF[0].WEBP[0], c = b.indexOf("\u009d\u0001*"),
+      e = 0, d = []; 4 > e; e++) d[e] = b.charCodeAt(c + 3 + e);
+    e = d[1] << 8 | d[0];
+    c = e & 16383;
+    e = d[3] << 8 | d[2];
+    return {
+      width: c,
+      height: e & 16383,
+      data: b,
+      riff: a
+    }
+  }
+
+  function k(a) {
+    for (var b = 0, c = {}; b < a.length;) {
+      var e = a.substr(b, 4),
+        d = parseInt(a.substr(b + 4, 4).split("").map(function(a) {
+          a = a.charCodeAt(0).toString(2);
+          return Array(8 - a.length + 1).join("0") + a
+        }).join(""), 2),
+        g = a.substr(b + 4 + 4, d),
+        b = b + (8 + d);
+      c[e] = c[e] || [];
+      "RIFF" == e || "LIST" == e ? c[e].push(k(g)) : c[e].push(g)
+    }
+    return c
+  }
+
+  function s(a) {
+    return [].slice.call(new Uint8Array((new Float64Array([a])).buffer),
+      0).map(function(a) {
+      return String.fromCharCode(a)
+    }).reverse().join("")
+  }
+
+  function l(a, b) {
+    this.frames = [];
+    this.duration = 1E3 / a;
+    this.quality = b || .8
+  }
+  l.prototype.add = function(a, b) {
+    if ("undefined" != typeof b && this.duration) throw "you can't pass a duration if the fps is set";
+    if ("undefined" == typeof b && !this.duration) throw "if you don't have the fps set, you ned to have durations here.";
+    "canvas" in a && (a = a.canvas);
+    if ("toDataURL" in a) a = a.toDataURL("image/webp", this.quality);
+    else if ("string" != typeof a) throw "frame must be a a HTMLCanvasElement, a CanvasRenderingContext2D or a DataURI formatted string";
+    if (!/^data:image\/webp;base64,/ig.test(a)) throw "Input must be formatted properly as a base64 encoded DataURI of type image/webp";
+    this.frames.push({
+      image: a,
+      duration: b || this.duration
+    })
+  };
+  l.prototype.compile = function(a) {
+    return new h(this.frames.map(function(a) {
+      var c = q(k(atob(a.image.slice(23))));
+      c.duration = a.duration;
+      return c
+    }), a)
+  };
+  return {
+    Video: l,
+    fromImageArray: function(a, b, c) {
+      return h(a.map(function(a) {
+        a = q(k(atob(a.slice(23))));
+        a.duration = 1E3 / b;
+        return a
+      }), c)
+    },
+    toWebM: h
+  }
+}();
+
+playground.VideoRecorder = function(app, args) {
+
+  this.app = app;
+
+};
+
+playground.VideoRecorder.prototype = {
+
+  setup: function(args) {
+
+    this.region = false;
+
+    playground.extend(this, {
+      followMouse: false,
+      framerate: 20,
+      scale: 1
+    }, args);
+
+    if (!this.region) {
+      this.region = [0, 0, this.app.layer.width, this.app.layer.height];
+    }
+
+    this.playbackRate = this.framerate / 60;
+
+    this.layer = cq(this.region[2] * this.scale | 0, this.region[3] * this.scale | 0);
+  },
+
+  start: function(args) {
+    this.setup(args);
+    this.encoder = new Whammy.Video(this.framerate);
+    this.captureTimeout = 0;
+    this.recording = true;
+  },
+
+  step: function(delta) {
+
+    if (this.encoder) {
+
+      this.captureTimeout -= delta * 1000;
+
+      if (this.captureTimeout <= 0) {
+        this.captureTimeout = 1000 / this.framerate + this.captureTimeout;
+
+        this.layer.drawImage(this.app.layer.canvas, this.region[0], this.region[1], this.region[2], this.region[3], 0, 0, this.layer.width, this.layer.height);
+        this.encoder.add(this.layer.canvas);
+      }
+
+      this.app.screen.save().lineWidth(8).strokeStyle("#c00").strokeRect(0, 0, this.app.screen.width, this.app.screen.height).restore();
+    }
+
+  },
+
+  stop: function() {
+    if (!this.encoder) return;
+    var output = this.encoder.compile();
+    var url = (window.webkitURL || window.URL).createObjectURL(output);
+    window.open(url);
+    this.recording = false;
+
+    delete this.encoder;
+  },
+
+  toggle: function(args) {
+    if (this.encoder) this.stop();
+    else this.start(args);
+  }
+
 };
