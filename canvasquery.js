05a62bf1dc5b822d25f0d8ebddce9e02db8bb472
rezoner <rezoner1337@gmail.com>
2016-02-16 16:09:43 +0100

    image loader

--- canvasquery.js
+++ canvasquery.js
@@ -40,6 +40,7 @@
   var COCOONJS = false;
 
   var Canvas = window.HTMLCanvasElement;
+  var orgImage = window.Image;
   var Image = window.HTMLImageElement;
   var ImageBitmap = window.ImageBitmap || window.HTMLImageElement;
   var COCOONJS = navigator.isCocoonJS;
@@ -108,6 +109,116 @@
 
   };
 
+  /*
+
+    cq.loadImages();
+
+    cq.run(function(){
+
+    });
+
+  */
+
+  /* Micro framework */
+
+  cq.images = {};
+  cq.atlases = {};
+  cq.loaderscount = 0;
+  cq.loadercallback = function() {
+
+    cq.loaderscount--;
+
+  };
+
+  cq.loadImages = function(keys) {
+
+    var promises = [];
+
+    for (var key in keys) {
+
+      cq.loaderscount++;
+
+      var path = keys[key];
+
+      var image = new orgImage();
+
+      cq.images[key] = image;
+      cq.loaderscount++;
+
+      var promise = new Promise(function(resolve, reject) {
+
+        image.addEventListener("load", function() {
+
+          cq.loadercallback();
+
+          resolve();
+
+        });
+
+        image.addEventListener("error", function() {
+
+          throw ("unable to load " + this.src);
+
+        });
+
+      });
+
+      image.src = path;
+
+    }
+
+    return Promise.all(promises);
+
+  };
+
+  cq.loadAtlases = function() {
+
+  };
+
+  /* WIP */
+
+  cq.run = function(callback) {
+
+    var lasTick = Date.now();
+
+    var frame = function() {
+
+      requestAnimationFrame(frame);
+
+      var dt = Date.now() - lastTick;
+      lastTick = Date.now();
+
+      if (cq.loaderscount === 0) callback(dt);
+
+    }
+
+    requestAnimationFrame(frame);
+
+  };
+
+  /* WIP */
+
+  cq.viewport = function() {
+
+    if (!cq.layer) {
+
+      cq.layer = cq();
+      cq.layer.appendTo(document.body);
+
+    }
+
+  };
+
+  /* WIP */
+  cq.mouse = function(callback) {
+
+    document.addEventListener('mousedown', function(e) {
+
+      console.log(e);
+
+    });
+
+  }
 
   cq.cocoon = function(selector) {
     if (arguments.length === 0) {
